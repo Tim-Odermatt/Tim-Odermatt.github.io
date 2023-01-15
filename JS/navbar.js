220973a588db1ef61818a48e69b9340e16b20442
Tim-Odermatt <tim.oderpo@gmail.com>
2023-01-15 03:33:56 +0100

    hotfix2

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,9 @@
 window.onscroll = function() {myFunction()};
-  
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
 
+window.onload = function() {
+  var navbar = document.getElementById("navbar");
+  var sticky = navbar.offsetTop;
+}
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
